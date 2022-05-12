1814f8f37a1491845fd1559c36eb250a47093fb6
gary <gary.white@lodgit.com>
2022-05-12 13:12:27 +0200

    code

---
+++ app.js
@@ -0,0 +1,11 @@
+// let lastLetter = card[card.length - 1]
+
+function remove(string) {
+    return '';
+}
+
+console.log(remove("Hi!")); // "Hi"
+console.log(remove("Hi!!!")); // "Hi"
+console.log(remove("!Hi")); // "!Hi"
+console.log(remove("!Hi!")); // "!Hi"
+console.log(remove("Hi! Hi!")); // "Hi! Hi"
